833c0d5fa5f1135beb04a48b4b804daac6740796
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-06-27 17:24:31 -0400

    finished

--- todo.sql
+++ todo.sql
@@ -7,5 +7,15 @@ CREATE TABLE todo (
   "completed_at" DATETIME
 );
 
-INSERT INTO todo (title, priority, created_at ) VALUES ('wash_dishes', 1, ),
-INSERT INTO todo ()
+INSERT INTO todo (title, priority, created_at, completed_at ) VALUES ('wash_dishes', 1,'2017-6-26 12:05:00',NULL);
+INSERT INTO todo (title, priority, created_at, completed_at) VALUES ('vacuum', 2, '2017-6-26 12:05:00', '2017-6-27 12:05:00');
+INSERT INTO todo (title, priority, created_at, completed_at) VALUES ('dust', 3, '2017-6-25 12:05:00', NULL);
+INSERT INTO todo (title, priority, created_at, completed_at) VALUES ('mop', 4, '2017-6-24 12:05:00', NULL);
+INSERT INTO todo (title, priority, created_at, completed_at) VALUES ('sweep', 5, '2017-5-01 12:05:00', NULL);
+
+SELECT * FROM todo WHERE completed_at = NULL;
+SELECT * FROM todo WHERE priority > 1;
+
+UPDATE todo SET completed_at = '2017-06-27 12:10:00' WHERE id = 1;
+
+DELETE FROM todo WHERE completed_at IS NOT NULL;
